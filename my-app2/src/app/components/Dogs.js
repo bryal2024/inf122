282571921735fa1266886413a6f2b76d84753d51
bryal2024 <157467564+bryal2024@users.noreply.github.com>
2024-01-24 15:32:55 +0000

    miercoles

---
+++ my-app2/src/app/components/Dogs.js
@@ -0,0 +1,17 @@
+"use client"
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "./Dogs.module.css";
+
+function Dog() {
+    const[dog, setDog]= useState("/vercel.svg");
+    const url = "https://dog.ceo/api/breed/terrier/irish/images/random";
+    useEffect(()=>{Fetch(url).then(res=>res.json()).then(data=>{setDog(data.message)})},[]);
+     return (
+        <div className={style.box}>
+            <h1>Dogs</h1>
+            <Image src={dog} alt="dog" width={500} height={500}  />
+        </div>
+    );
+}
+export default Dog;
---
+++ my-app2/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Dogs from "./components/Dogs";
+export default function Home() {
+  return (
+    <main className={styles.main}>
+        <Dogs/>
+    </main>
+  );
+}
